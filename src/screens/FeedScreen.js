8a45c8035119afef1fbe0da9bf5854c6d2d679a3
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2024-06-23 03:56:24 -0700

    feed ui

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -2,16 +2,21 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './FeedScreen.css';
+import '../variables.css'
 
 function FeedScreen() {
     const location = useLocation();
     const { articles } = location.state;
 
+    const column1 = articles.slice(0, Math.ceil(articles.length / 2));
+    var temp = column1.length;
+    const column2 = articles.slice(Math.ceil(articles.length / 2));
+
     return (
         <div className="feed-screen">
             <div className="article-grid">
-                {articles.map((article, index) => (
-                    <div key={index} className={`article-card ${index % 3 === 0 ? 'larger' : ''}`}>
+                {/*articles.map((article, index) => (
+                    <div key={index} className={`article-card`}>
                         <img
                             src={article.imageLink}
                             alt={`Article ${index + 1}`}
@@ -20,7 +25,31 @@ function FeedScreen() {
                         <h2 className="article-headline">{article.headline}</h2>
                 
                     </div>
+                ))*/}
+                <div className="grid-column">
+                 {column1.map((article, index) => (
+                    <div key={index} className="article-card">
+                        <img
+                            src={article.imageLink}
+                            alt={`Article ${index + 1}`}
+                            className="article-image"
+                        />
+                        <h2 className="article-headline">{article.headline}</h2>
+                    </div>
                 ))}
+                </div>
+                <div className="grid-column">
+                 {column2.map((article, index) => (
+                    <div key={index + temp} className="article-card">
+                        <img
+                            src={article.imageLink}
+                            alt={`Article ${index + temp + 1}`}
+                            className="article-image"
+                        />
+                        <h2 className="article-headline">{article.headline}</h2>
+                    </div>
+                ))}
+                </div>
         </div>
     </div>
     );
