cbf96d5ad90d52c19f5cbfb28b074726b1eea6ab
lfedronic <lfedronic@ucdavis.edu>
2024-06-22 17:10:39 -0700

    added questionaire

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
+import HomePage from './components/HomePage.js';
 import './App.css';
 import axios from 'axios';
 
@@ -19,19 +20,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">  
+        <HomePage>/</HomePage>
         <input 
           type="text" 
           value={prompt} 
